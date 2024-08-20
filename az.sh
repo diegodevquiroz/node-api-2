8231cb9717c4f2984de7c827b40549b44b4197f2
Diego Antonio Quiroz Ramirez <ingquirozramirez@gmail.com>
2024-08-20 14:25:53 -0500

    just node

--- az.sh
+++ az.sh
@@ -45,6 +45,23 @@ get_client_id() {
   fi
 }
 
+
+config_contributor(){
+    az ad sp create-for-rbac --name "github-action-sp" --role contributor --scopes /subscriptions/$subscriptionId
+}
+
+config_OIDC() {
+    local appId='143bf878-5f76-4280-b04b-642fdd558ca9'
+    local organization='diegodevquiroz'
+    local nameRepo='node-api-2'
+    local env='Production'
+
+
+    az ad app federated-credential create --id $appId \
+    --parameters "{\"name\":\"GitHubActions\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:$organization/$nameRepo:environment:$env\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+        
+}
+
 # Function to set up Azure resources
 setup_azure_resources() {
     local group=$1
@@ -64,17 +81,23 @@ setup_azure_resources() {
     #Comentar la linea si no se desea crear un App Service Plan
     az appservice plan create --name $plan --resource-group $group --sku B1 --is-linux
 
-    #echo "Creating Web App..."
-    #az webapp create --resource-group $group --plan $plan --name $nameApp --runtime "$runtime"
+    echo "Creating Web App..."
+    az webapp create --resource-group $group --plan $plan --name $nameApp --runtime "$runTime"
 }
 
 # Main function
 main() {
-#   # Set up Azure resources
-#   setup_azure_resources 'group003' 'eastus' 'plan003' 'appname03' 'NODE|18-lts'
+    # Set up Azure resources
+    setup_azure_resources 'group003' 'eastus' 'plan003' 'appNameDiego22' 'NODE|18-lts'
  
-#   # Fetch the Subscription ID
-#   get_subscription_id
+    # Fetch the Subscription ID
+    get_subscription_id
+
+    # Set Contributor
+    config_contributor
+
+    #Set OIDC
+    config_OIDC
 
 #   # Fetch the Tenant ID
 #   get_tenant_id
@@ -87,10 +110,12 @@ main() {
 #   echo "Subscription ID: $subscriptionId"
 #   echo "Tenant ID: $tenantId"
 #   echo "Client ID: $clientId"
-    #az login
-    az ad app federated-credential create --id 143bf878-5f76-4280-b04b-642fdd558ca9 \
-    --parameters "{\"name\":\"GitHubActions\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:diegoquirozramirez/node-api-2:ref:refs/heads/main\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
+
+
+    # az ad app federated-credential create --id 143bf878-5f76-4280-b04b-642fdd558ca9 \
+    # --parameters "{\"name\":\"GitHubActions\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:diegodevquiroz/node-api-2:environment:Production\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+        
     #get_subscription_id
     #az ad sp create-for-rbac --name "github-action-sp" --role contributor --scopes /subscriptions/$subscriptionId
 
